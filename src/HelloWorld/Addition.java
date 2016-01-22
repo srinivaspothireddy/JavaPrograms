46d94088915d6993bdd977b5aa0284b47efb85ff
Srinivas Pothireddy <spothireddy@spothireddy-mba.local>
2016-01-22 12:06:33 -0500

    Added Addition.java and printed the result in HelloWorld

---
+++ src/HelloWorld/Addition.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Addition {
+	int x = 10;
+	int y = 20;
+	
+	int z = x+y;
+	
+
+	
+
+}
--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,13 +1,14 @@
 package HelloWorld;
 
 public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String args[]){
+		Addition add = new Addition();
 		System.out.println("Hello World");
 		System.out.println("Hello World1");
 		System.out.println("Hello World2");
-
+		
+		System.out.println(add.z);
+		
 	}
 
 }
